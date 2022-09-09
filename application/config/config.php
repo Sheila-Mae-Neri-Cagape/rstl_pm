370b04aa58b33c7d171ddc20cab3abdb40071c18
Windyl Joseph A. Khu <windylkhu@gmail.com>
2022-09-09 13:37:57 +0800

    test

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = ($_SERVER['HTTP_HOST'] == 'pm.region10.dost.gov.ph' ? 'https://'.$_SERVER['HTTP_HOST'].'/rstl_pm/' : 'http://'.$_SERVER['HTTP_HOST'].'/rstl_pm/');
-
+//$config['base_url'] = ($_SERVER['HTTP_HOST'] == 'pm.region10.dost.gov.ph' ? 'https://'.$_SERVER['HTTP_HOST'].'/rstl_pm/' : 'http://'.$_SERVER['HTTP_HOST'].'/rstl_pm/');
+$config['base_url'] = 'http://122.53.79.156:8082/rstl_pm/';
 /*
 |--------------------------------------------------------------------------
 | Index File
